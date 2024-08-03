098c06d7df3cd38511abda1a1a832d6ac0db96ad
Your Name <you@example.com>
2024-08-04 01:35:44 +0530

    add database with server

---
+++ server/controller/userController.js
@@ -0,0 +1,35 @@
+// controllers/authController.js
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({ username, email, password });
+    await user.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(400).json({ error: "Error creating user" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
-import express from "express";
+// app.js
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dbconfig from './db/db.js';
+
+
+
+dotenv.config();
+
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port , () =>{
-    console.log("Server is running on this port numbers ", port);
-})
+// app.use('/api/auth', router);
+
+dbconfig()
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
